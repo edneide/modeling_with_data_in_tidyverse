e25c2016fc5f5084bf69222bbbb8706b0c0b87b7
edneide <edneide.ramalho@gmail.com>
2021-12-02 15:46:54 -0300

    creating first script

---
+++ 001_intro_to_modeling.R
@@ -0,0 +1,12 @@
+# 0. Packages --------
+library(dplyr)
+library(moderndive)
+library(ggplot2)
+glimpse(evals)
+
+# 1. Exploratory Data Analysis (EDA) ------------
+
+# 1. Looking at the data
+# 2. Creating Visualiztions
+# 3. Computing summary statistics
+
