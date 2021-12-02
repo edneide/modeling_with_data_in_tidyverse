d622bbc00da34bdf594c3e50b512dd9d343e2853
edneide <edneide.ramalho@gmail.com>
2021-12-02 15:48:46 -0300

    update

--- 001_intro_to_modeling.R
+++ 001_intro_to_modeling.R
@@ -1,12 +1,22 @@
 # 0. Packages --------
 library(dplyr)
 library(moderndive)
-library(ggplot2)
-glimpse(evals)
+library(tidyverse)
+
 
 # 1. Exploratory Data Analysis (EDA) ------------
 
 # 1. Looking at the data
-# 2. Creating Visualiztions
-# 3. Computing summary statistics
+glimpse(evals)
 
+# 2. Creating Visualiztions
+evals %>% 
+  ggplot(aes(x = score)) +
+  geom_histogram(binwidth = 0.25, color = "black") +
+  labs(x = "teaching score", y = "count")
+
+# 3. Computing summary statistics
+evals %>% 
+  summarize(mean_score = mean(score),
+            median_score = median(score),
+            sd_score = sd(score))
