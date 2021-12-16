60283325610d1030e0b303f448a0997b22cd781c
edneide <edneide.ramalho@gmail.com>
2021-12-16 14:19:29 -0300

    adding content

--- 001_intro_to_modeling.R
+++ 001_intro_to_modeling.R
@@ -6,17 +6,19 @@ library(tidyverse)
 
 # 1. Exploratory Data Analysis (EDA) ------------
 
-# 1. Looking at the data
+## 1.1. Looking at the data ----------
 glimpse(evals)
 
-# 2. Creating Visualizations
+## 1.2. Creating Visualizations ----------
 evals %>% 
   ggplot(aes(x = score)) +
   geom_histogram(binwidth = 0.25, color = "black") +
   labs(x = "teaching score", y = "count")
 
-# 3. Computing summary statistics
+## 1.3. Computing summary statistics ----------
 evals %>% 
   summarize(mean_score = mean(score),
             median_score = median(score),
             sd_score = sd(score))
+
+# 2. Background on modeling for prediction ---------
